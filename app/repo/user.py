4b02dba6e5de05ad87a6fd908736a9cad860aef7
Gudini <gudini108@gmail.com>
2023-08-22 14:47:06 +0300

    added userrepo

---
+++ app/repo/user.py
@@ -0,0 +1,16 @@
+from app.db import TodoUser
+from app.schemas import TodoUserInput
+
+
+class UserRepo():
+    @staticmethod
+    async def safe_get_user_by_username(username: str) -> TodoUser | None:
+        return await TodoUser.objects.get_or_none(username=username)
+
+    @staticmethod
+    async def get_all_users():
+        return await TodoUser.objects.all()
+
+    @staticmethod
+    async def save_user(user_input: TodoUserInput):
+        return await TodoUser.objects.create(**user_input.dict())
